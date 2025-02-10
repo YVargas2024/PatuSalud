681e6fd5d247096f58895814adf6751f881f8e8d
YVargas2024 <yulian.vargas@genstudents.org>
2025-02-10 14:48:00 -0500

    cp

--- js/perfil.js
+++ js/perfil.js
@@ -20,3 +20,12 @@ function moverCarrusel(direccion) {
     // Cambiar la posición del carrusel
     tareas.style.transform = `translateX(-${carruselIndex * servicioWidth}px)`;
 }
+// Función para mostrar el modal
+function mostrarModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Función para cerrar el modal
+function cerrarModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
